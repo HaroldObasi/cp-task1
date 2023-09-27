d8be35ed54a1081981dc176a250bd48482461b07
HaroldObasi <haroldobasi2k16@gmail.com>
2023-09-27 23:58:37 +0100

    added portal div and context

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 import Sidebar from "./components/Sidebar";
 import MainContent from "./components/MainContent";
+import Modal from "./components/ui/Modal";
 
 export default function Home() {
   return (
-    <main className="flex flex-row min-h-screen w-full">
+    <main className="flex flex-row min-h-screen w-full max-h-screen">
       <Sidebar />
       <MainContent />
+      <Modal />
     </main>
   );
 }
