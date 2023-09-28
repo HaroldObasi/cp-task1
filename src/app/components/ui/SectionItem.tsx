ab833f84073d2c8421e700cc6538e805f7a4ce3c
HaroldObasi <haroldobasi2k16@gmail.com>
2023-09-28 16:18:40 +0100

    section Item component changes

--- src/app/components/ui/SectionItem.tsx
+++ src/app/components/ui/SectionItem.tsx
@@ -3,14 +3,17 @@ import React from "react";
 type Props = {
   children?: React.ReactNode;
   title: string;
+  className?: string;
+  innerBodyStyling?: string;
 };
 
 const SectionItem = (props: Props) => {
   return (
-    <div className="w-[595px] shadow-lg rounded-[20px]">
-      {/* <div className="bg-[#D0F7FA] py-[25px] pl-[32px]"></div> */}
-      <p className="bg-[#D0F7FA] py-[25px] pl-[32px]">{props.title}</p>
-      <div className="px-[40px]">{props.children}</div>
+    <div className={"w-[595px] shadow-lg rounded-[20px] mb-5 overflow-hidden"}>
+      <p className="bg-[#D0F7FA] py-[25px] pl-[32px] text-[25px]">
+        {props.title}
+      </p>
+      <div className={"px-[40px] py-[60px] bg-white"}>{props.children}</div>
     </div>
   );
 };
