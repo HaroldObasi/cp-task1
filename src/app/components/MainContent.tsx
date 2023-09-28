aeeb4c1b3174ccdad9e78a1675ca99b11e3db76d
HaroldObasi <haroldobasi2k16@gmail.com>
2023-09-28 23:16:20 +0100

    removed unnecessary state variable

--- src/app/components/MainContent.tsx
+++ src/app/components/MainContent.tsx
@@ -1,9 +1,6 @@
 import React, { useEffect, useState, useContext } from "react";
 import axios from "axios";
-import SectionItem from "./ui/SectionItem";
-import Uploader from "./ui/Uploader";
-import Form from "./ui/Form";
-import FormItem from "./ui/Form/FormItem";
+
 import { ApplicationForm } from "../types/ApplicationForm";
 import { ModalContext } from "@/context/modal.context";
 import PersonalInformation from "./sections/PersonalInformation";
@@ -12,15 +9,13 @@ import AdditionalQuestions from "./sections/AdditionalQuestions";
 import CoverImage from "./sections/CoverImage";
 
 const MainContent = () => {
-  const { state, dispatch } = useContext(ModalContext);
-  const [defaultForm, setDefaultForm] = useState<ApplicationForm | null>(null);
+  const { dispatch } = useContext(ModalContext);
   const fetchForm = async () => {
     try {
       const response = await axios.get(
         "http://127.0.0.1:4010/api/819.6588064757781/programs/qui/application-form"
       );
       const data: ApplicationForm = response.data;
-      setDefaultForm(data);
       dispatch({
         type: "SET_FORM_ATTRS",
         defaultFormAttributes: data.data.attributes,
