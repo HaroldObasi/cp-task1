fd27ea080f369d155918a1f69e332a9f21ea6fbc
HaroldObasi <haroldobasi2k16@gmail.com>
2023-09-28 16:17:40 +0100

    component for form item

---
+++ src/app/components/ui/Form/FormItem/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import {
+  QuestionTemplate,
+  PersonalInformationTemplate,
+  ProfileTemplate,
+} from "@/app/types/ApplicationForm";
+
+type Props = {
+  name: string;
+  key: string;
+  internal?: boolean;
+  show?: boolean;
+  //   data:
+  //     | QuestionTemplate[]
+  //     | PersonalInformationTemplate
+  //     | ProfileTemplate
+  //     | undefined;
+};
+
+const index = (props: Props) => {
+  return (
+    <div className="pb-[25px] border-b mb-[23px]  border-[#C4C4C4]">
+      <p className="font-semibold text-xl">{props.name}</p>
+    </div>
+  );
+};
+
+export default index;
