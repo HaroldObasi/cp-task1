ac4849ae3d2ef674caf49dcc09536b2c9bfaa151
HaroldObasi <haroldobasi2k16@gmail.com>
2023-09-29 13:13:34 +0100

    added maxheight option specifically for modal

--- src/app/components/ui/SectionItem.tsx
+++ src/app/components/ui/SectionItem.tsx
@@ -5,11 +5,16 @@ type Props = {
   title: string;
   className?: string;
   innerBodyStyling?: string;
+  fullHeight?: boolean;
 };
 
 const SectionItem = (props: Props) => {
   return (
-    <div className={"w-[595px] shadow-lg rounded-[20px] mb-5 overflow-hidden"}>
+    <div
+      className={`${
+        props.fullHeight === false && "max-h-[487px] overflow-y-auto"
+      } w-[595px] shadow-lg rounded-[20px] mb-5 overflow-hidden`}
+    >
       <p className="bg-[#D0F7FA] py-[25px] pl-[32px] text-[25px]">
         {props.title}
       </p>
